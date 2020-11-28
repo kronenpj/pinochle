3ce2f01b87a4631588fa84ea451c19055a987ee4
Paul Kronenwetter <kronenpj@gmail.com>
2020-11-28 15:55:42 -0500

    Forcing use of Python 3.8 if it exists because VSCode's python extensions don't support 3.9.

--- create_venv.sh
+++ create_venv.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-/usr/bin/python3 -m venv venv --clear --prompt pinochle
+if [ -x /usr/bin/python3.8 ];then
+  PY=python3.8
+else
+  PY=python3
+fi
+
+/usr/bin/${PY} -m venv venv --clear --prompt pinochle
 source venv/bin/activate
 pip install -r requirements.txt -r requirements-test.txt
