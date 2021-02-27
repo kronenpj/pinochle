ed517cf0f2bb0e3175a429956ebf6917fc25c895
Paul Kronenwetter <kronenpj@gmail.com>
2021-02-27 17:26:53 -0500

    Update from brySVG author and associated changes. Promising!

--- pinochle/static/dragdrop2.py
+++ pinochle/static/dragdrop2.py
@@ -1,51 +1,39 @@
-# import math
 from sys import stderr
 
-from browser import document, svg
+from browser import document
 import brySVG.polygoncanvas as SVG
 
-def checkposition(event):
-    # event.preventDefault()
-    pass
-
-def clearintersections(event):
-    # event.allowDefault()
-    pass
-
-# SVGRoot = document["svg_root"]
+# Locate the card table in the HTML document.
 SVGRoot = document["card_table"]
 
 # Create the base SVG object for the card table.
-canvas = SVG.CanvasObject("95vw", "95vh", "cyan", objid="canvas")
+canvas = SVG.CanvasObject("95vw", "95vh", "green", objid="canvas")
 SVGRoot <= canvas
-
 canvas.mouseMode = SVG.MouseMode.DRAG
 
-# this will serve as the canvas over which items are dragged.
-# having the drag events occur on the mousemove over a backdrop
-# (instead of the dragged element) prevents the dragged element
-# from being inadvertantly dropped when the mouse is moved rapidly
-# outline = SVG.PolygonObject([(180, 100), (280,100), (280,200), (180,200)], fillcolour="none")
-# canvas.addObject(outline, fixed=True)
+# Intrinsic dimensions of the cards in the deck.
+card_width = 170
+card_height = 245
 
+# Gather information about the display environment
+table_width = document["card_table"].clientWidth
+table_height = document["card_table"].clientHeight
 
-xincr = 60
-yincr = 80
-xpos = 0
-ypos = 0
+# Calculate relative increments for the card table.
+xincr = int(table_width / 13)
+yincr = int(card_height / 4)
 
-for card in ["ace", "king", "queen", "jack"]:
+# Where to start the first card on the table.
+(xpos, ypos) = (0, 0)
+
+# Fan out the cards.
+for card in ["ace", "10", "king", "queen", "jack", "9"]:
     for suit in ["heart", "diamond", "spade", "club"]:
-        piece = svg.use(
-            href="/static/svg-cards.svg#" + f"{suit}_{card}"
-            # , x=xpos, y=ypos
-            #, transform="scale(0.5)"
-        )
-        canvas.addObject(piece)
-        # canvas.translateObject(piece, (xpos, ypos))
-        canvas.translateElement(piece, (xpos, ypos))
-        xpos += xincr
-    # xpos = 0
-    # ypos += yincr
-
-# canvas.fitContents()
+        for decks in range(0, 2):  # Double decks
+            piece = SVG.UseObject(href="/static/svg-cards.svg#" + f"{suit}_{card}")
+            canvas.addObject(piece)
+            canvas.translateObject(piece, (xpos, ypos))
+            xpos += xincr
+        if xpos > table_width - card_width:
+            xpos = 0
+            ypos += yincr
