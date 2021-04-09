6e166ff1764c8ca7d0ba679a9cf10542c232f311
Paul Kronenwetter <kronenpj@gmail.com>
2021-04-09 10:06:16 -0400

    Minor update to gitignore and coverage.sh.

--- coverage.sh
+++ coverage.sh
@@ -4,6 +4,15 @@ if [ -z "$VIRTUAL_ENV" ]; then
   . venv/bin/activate
 fi
 
+if [ -d src/pinochle/instance ]; then
+  MOVED=1
+  mv src/pinochle/instance src
+fi
+
 coverage run -m pytest -m 'not hypothesis' "$@"
 
 coverage xml
+
+if [ ! -z "$MOVED" ]; then
+  mv src/instance src/pinochle
+fi
