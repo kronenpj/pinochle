7b85160cc7014582bd3f46594417a84c3b286d8f
Paul Kronenwetter <kronenpj@gmail.com>
2021-06-11 13:43:55 -0400

    Moving log initialization to init from main.

--- src/pinochle/__init__.py
+++ src/pinochle/__init__.py
@@ -1,5 +1,25 @@
 import logging
+from os import environ
+from sys import stdout
 
 name = "pinochle"
 
-GLOBAL_LOG_LEVEL = logging.WARNING
+GLOBAL_LOG_LEVEL: int
+if "GLOBAL_LOG_LEVEL" in environ:
+    print("Setting GLOBAL_LOG_LEVEL from environment.")
+    GLOBAL_LOG_LEVEL = int(environ["GLOBAL_LOG_LEVEL"])
+else:
+    GLOBAL_LOG_LEVEL = logging.WARNING
+
+def setup_logging() -> logging.Logger:
+    root_logger = logging.getLogger()
+    root_logger.setLevel(GLOBAL_LOG_LEVEL)
+
+    handler = logging.StreamHandler(stdout)
+    handler.setLevel(root_logger.getEffectiveLevel())
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    root_logger.addHandler(handler)
+    return root_logger
--- src/pinochle/__main__.py
+++ src/pinochle/__main__.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python3
 
-import logging
-import sys
-
 from . import wsgi  # pragma: no cover
-from .__init__ import GLOBAL_LOG_LEVEL
+from .__init__ import setup_logging
+
 
 if __name__ == "__main__":
-    ROOT_LOGGER = logging.getLogger()
-    ROOT_LOGGER.setLevel(GLOBAL_LOG_LEVEL)
-
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(ROOT_LOGGER.getEffectiveLevel())
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    handler.setFormatter(formatter)
-    ROOT_LOGGER.addHandler(handler)
+    ROOT_LOGGER = setup_logging()
 
     # wsgi.application.run(
     #     host="0.0.0.0", port=5000, use_reloader=True, use_debugger=True, threaded=True
