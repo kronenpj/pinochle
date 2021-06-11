2f32b9929590b8048e0921b3854edbc1e286f95e
Paul Kronenwetter <kronenpj@gmail.com>
2021-06-11 15:39:39 -0400

    Adding progress messages. Changing to python3.9 as a default, if it's found.

--- create_venv.bat
+++ create_venv.bat
@@ -1,7 +1,11 @@
 @echo off
 
+echo "Clearing and rebuilding .venv"
 python3 -m venv .venv --clear --prompt pinochle
+echo "Activating .venv"
 .venv\scripts\activate.bat
+echo "Installing poetry."
 pip3 install poetry
 
+echo Installing development dependencies.
 poetry install --no-root
--- create_venv.sh
+++ create_venv.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-if [ -x /usr/bin/python3.8 ];then
-  PY=python3.8
+if [ -x /usr/bin/python3.9 ];then
+  PY=python3.9
 else
   PY=python3
 fi
 
+echo "Clearing and rebuilding .venv"
 /usr/bin/${PY} -m venv .venv --clear --prompt pinochle
+echo "Activating .venv"
 source .venv/bin/activate
+echo "Installing poetry"
 pip3 install poetry
 #pip install --use-feature=2020-resolver -r requirements.txt -r requirements-test.txt
+echo "Installing (development) requirements"
 /usr/bin/poetry install --no-root
