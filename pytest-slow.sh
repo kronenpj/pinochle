d5a472c2b33170452511c75edda8a5900d3c2f35
Paul Kronenwetter <kronenpj@gmail.com>
2021-07-09 14:23:11 -0400

    Removing export.

--- pytest-slow.sh
+++ pytest-slow.sh
@@ -14,7 +14,7 @@ fi
 export COLLECT_VIDEO=${COLLECT_VIDEO-False}
 export COLLECT_BROWSER_LOGS=${COLLECT_BROWSER_LOGS-False}
 if [ -z "$SERVER_IP" ]; then
-  export _SUBNET=$(ip r | grep default | awk '{print $3}' | sed -E -e 's/\.[0-9]+?$//')
+  _SUBNET=$(ip r | grep default | awk '{print $3}' | sed -E -e 's/\.[0-9]+?$//')
   export SERVER_IP=$(ip -4 a | grep $_SUBNET | awk '{print $2}' | sed -e 's%/24%%')
 else
   export SERVER_IP=$SERVER_IP
