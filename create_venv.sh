1b0b25eaba5136c1d0c66f9b9a137957a09a697f
Paul Kronenwetter <kronenpj@gmail.com>
2021-08-06 10:12:42 -0400

    Updating create venv scripts.

--- create_venv.sh
+++ create_venv.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-if [ -x /usr/bin/python3.9 ];then
-  PY=python3.9
+PY_BIN=${1:-python3.9}
+
+if [ -x /usr/bin/${PY_BIN} ];then
+  PY=${PY_BIN}
 else
-  PY=python3
+  PY=/usr/bin/python3
 fi
 
 echo "Clearing and rebuilding .venv"
 /usr/bin/${PY} -m venv .venv --clear --prompt pinochle
+
 echo "Activating .venv"
 source .venv/bin/activate
+
 echo "Installing poetry"
 pip3 install poetry
-#pip install --use-feature=2020-resolver -r requirements.txt -r requirements-test.txt
+
 echo "Installing (development) requirements"
 poetry install --no-root
