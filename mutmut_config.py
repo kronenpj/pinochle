f6f124760776bb20925ac90ea9137410fcf96e68
Paul Kronenwetter <kronenpj@gmail.com>
2021-08-06 10:15:45 -0400

    More changes to mutmut's behavior.

--- mutmut_config.py
+++ mutmut_config.py
@@ -2,28 +2,36 @@
 
 
 def pre_mutation(context):
-    strings = [
-        r"argp.add_argument",
-        r"argparse.ArgumentParser",
-        r"assert",
-        r"_get_input",
-        r"help=",
-        r"if __name__ ==",
-        r"InvalidDeckError",
-        r"InvalidSuiteError",
-        r"InvalidValueError",
-        r"is not an instance of",
-        r"jokers=",
-        r"Joker",
-        r"List",
-        r"log",
-        r"mylog",
-        r"outstream.write",
-        r"print",
-        r"raise",
-        r"sys.exit",
-        r"Union",
+    skip_source = [
+        "src/pinochle/custom_log.py",
+        "src/pinochle/log_decorator.py",
     ]
+    strings = [
+        "_get_input",
+        "argp.add_argument",
+        "argparse.ArgumentParser",
+        "assert",
+        "help=",
+        "if __name__ ==",
+        "InvalidDeckError",
+        "InvalidSuiteError",
+        "InvalidValueError",
+        "is not an instance of",
+        "Joker",
+        "jokers=",
+        "List",
+        "log",
+        "LOG",
+        "mylog",
+        "outstream.write",
+        "print",
+        "raise",
+        "sys.exit",
+        "Union",
+    ]
+    if context.filename in skip_source:
+        context.skip = True
+        return
     for string in strings:
         if string in context.current_source_line:
             context.skip = True
